b7ad9a8b47777dc7a6066ad52596502a9f23f900
pKrumov <petarkrumov90@gmail.com>
2019-11-17 21:02:00 +0200

    Add files via upload

---
+++ 03. Nums 1...20 .cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()  {
+
+    int a;
+    int h;
+    int area;
+
+    cin >> a >> h;
+    area = a * h;
+    cout << area << endl;
+
+    return 0;
+
+}
